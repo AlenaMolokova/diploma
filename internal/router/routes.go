9f2ac1b3cefc44f96894f370d1a2817b04de3743
AlenaMolokova <alena.v.molokova@gmail.com>
2025-04-29 05:21:51 +0300

    исправление ошибки с юзкейс

--- internal/router/routes.go
+++ internal/router/routes.go
@@ -5,6 +5,7 @@ import (
 	"github.com/AlenaMolokova/diploma/internal/loyalty"
 	"github.com/AlenaMolokova/diploma/internal/middleware"
 	"github.com/AlenaMolokova/diploma/internal/storage"
+	"github.com/AlenaMolokova/diploma/internal/usecase"
 	"github.com/go-chi/chi/v5"
 )
 
@@ -22,16 +23,20 @@ func SetupRoutes(store *storage.Storage, jwtSecret, loyaltyURL string) *chi.Mux 
 	r := chi.NewRouter()
 	loyaltyClient := loyalty.NewClient(loyaltyURL)
 
+	balanceUC := usecase.NewBalanceUseCase(store)
+	withdrawalUC := usecase.NewWithdrawalUseCase(store, balanceUC)
+	orderUC := usecase.NewOrderUseCase(store, loyaltyClient, balanceUC)
+
 	r.Post(UserPrefix+RegisterPath, handlers.NewRegisterHandler(store, jwtSecret).ServeHTTP)
 	r.Post(UserPrefix+LoginPath, handlers.NewLoginHandler(store, jwtSecret).ServeHTTP)
 
 	r.Group(func(r chi.Router) {
 		r.Use(middleware.AuthMiddleware(jwtSecret))
-		r.Post(UserPrefix+OrdersPath, handlers.NewOrderHandler(store, store, loyaltyClient).ServeHTTP)
+		r.Post(UserPrefix+OrdersPath, handlers.NewOrderHandler(orderUC).ServeHTTP)
 		r.Get(UserPrefix+OrdersPath, handlers.NewOrderGetHandler(store).ServeHTTP)
-		r.Get(UserPrefix+BalancePath, handlers.NewBalanceHandler(store).ServeHTTP)
-		r.Post(UserPrefix+WithdrawPath, handlers.NewWithdrawHandler(store, store).ServeHTTP)
-		r.Get(UserPrefix+WithdrawalsPath, handlers.NewWithdrawalsHandler(store).ServeHTTP)
+		r.Get(UserPrefix+BalancePath, handlers.NewBalanceHandler(balanceUC).ServeHTTP)
+		r.Post(UserPrefix+WithdrawPath, handlers.NewWithdrawHandler(withdrawalUC).ServeHTTP)
+		r.Get(UserPrefix+WithdrawalsPath, handlers.NewWithdrawalsHandler(withdrawalUC).ServeHTTP)
 	})
 
 	return r
